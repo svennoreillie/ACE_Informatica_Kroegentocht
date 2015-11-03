02ee7969c994297a94cabc2dc7ac5fb3cc09cf3c
Peter <Peter@192.168.47.29>
2015-11-03 21:20:50 +0100

    Factories Done, not tested yet

---
+++ Kroegentocht/src/helpers/factories/EstablishmentFactory.java
@@ -0,0 +1,27 @@
+/**
+ * @Autor: Peter Vervoort
+ * @Team: Team13
+ * @Date: 03/11/2015
+ * @Project: KroegenTocht
+ * @Purpose: Auto generator voor random Etablishment objecten(geg etc afkomstig uit enums)
+ */
+
+package helpers.factories;
+
+import java.util.Random;
+import helpers.enums.*;
+import model.Establishment;
+import model.TypeOfBusiness;
+
+public class EstablishmentFactory {
+
+	public static Establishment getEtablischement(Random rand){
+		Establishment establishment = new Establishment();
+		establishment.setAddress(AddressFactory.getAddress(rand));
+		TypeOfBusiness typeOfBusiness = new TypeOfBusiness();
+		EnumTypeOfBusiness enumType = EnumTypeOfBusiness.values()[rand.nextInt(EnumTypeOfBusiness.values().length)];
+		typeOfBusiness.setType(enumType.getTypeOfBusiness());
+		establishment.setBusinessType(typeOfBusiness);
+		return establishment;
+	}
+}
--- Kroegentocht/src/helpers/factories/VisitFactory.java
+++ Kroegentocht/src/helpers/factories/VisitFactory.java
@@ -3,11 +3,28 @@
  * @Team: Team13
  * @Date: 03/11/2015
  * @Project: KroegenTocht
- * @Purpose: Factory to auto generate visit objects
+ * @Purpose: Factory to auto generate visit objects (date: max day 28 & between 2009 and 2014)
  */
 
 package helpers.factories;
 
-public class VisitFactory {
+import java.util.GregorianCalendar;
+import java.util.Random;
+import model.Visit;
 
+public class VisitFactory {
+	
+	public static Visit getVisit(Random rand){
+		Visit visit = new Visit();
+		visit.setEstablishment(EstablishmentFactory.getEtablischement(rand));
+		int day, month, year;
+		day = 1+rand.nextInt(28);
+		month = 1+rand.nextInt(12);
+		year = 2014-rand.nextInt(5);
+		GregorianCalendar date = new GregorianCalendar(day,month,year);
+		visit.setDate(date);
+		visit.setAmountOfConsumptions(1+rand.nextInt(30));
+		visit.setDurationMinutes(1+rand.nextInt(480));
+		return visit;
+	}
 }
